7001b104c82be67e832773d2f1500219baa6cf60
Concsumer <157326516+Makshime@users.noreply.github.com>
2026-08-12 02:00:34 -0700

    Delete obj directory

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace WKRando
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "WKRando";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}

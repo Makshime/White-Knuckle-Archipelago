8d4b77ba0b0906766959d636f067e19cbd3f4160
Makshime <157326516+Makshime@users.noreply.github.com>
2026-08-12 17:28:46 -0700

    Added basic APWorld files

--- APWorld/options.py
+++ APWorld/options.py
@@ -2,3 +2,19 @@ from dataclasses import dataclass
 
 from Options import Choice, OptionGroup, PerGameCommonOptions, Range, Toggle
 
+class TestOption(Toggle):
+    """
+    A test option meant to check if the options menu works
+    """
+
+    display_name = "Test Option"
+
+class WKOptions(PerGameCommonOptions):
+    test_option = TestOption
+
+    option_groups = [
+        OptionGroup(
+            "Gameplay Options",
+            [TestOption],
+        )
+    ]
